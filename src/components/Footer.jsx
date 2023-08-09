3bd474bddd64dc4efb1c3577e486e2c0efda39af
erubae <115371540+ekedayen-e@users.noreply.github.com>
2023-08-09 13:08:27 +0100

    Changed look

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='border-t-1 z-20 text-center bg-white dark:bg-black'>
+    <div className='border-t-1 z-20 text-center'>
     <div className='flex justify-center items-center gap-5 py-2'>
-        <a href={'https://instagram.com/'}><i className="fa-brands text-lg sm:text-2xl fa-instagram duration-300  dark:hover:text-white hover:scale-110 cursor-pointer"></i></a>
-        <a href={'https://github.com/ekedayen-e'}><i className="fa-brands text-lg sm:text-2xl fa-github hover:scale-110  dark:hover:text-white cursor-pointer"></i></a>
-        <a href={'https://www.linkedin.com/in/ekedayen-e/'}> <i className="fa-brands text-lg sm:text-2xl fa-linkedin dark:hover:text-white hover:scale-110 cursor-pointer"></i></a>
+        <a href={'https://instagram.com/'}><i className="duration-300 fa-brands text-lg sm:text-2xl fa-instagram dark:hover:text-white hover:scale-110 cursor-pointer"></i></a>
+        <a href={'https://github.com/ekedayen-e'}><i className="duration-300 fa-brands text-lg sm:text-2xl fa-github hover:scale-110  dark:hover:text-white cursor-pointer"></i></a>
+        <a href={'https://www.linkedin.com/in/ekedayen-e/'}> <i className="duration-300 fa-brands text-lg sm:text-2xl fa-linkedin dark:hover:text-white hover:scale-110 cursor-pointer"></i></a>
     </div>
     © TODO
     </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 const Layout = ({children}) => {
   return (
-    <div className='duration-1000 relative min-h-screen flex flex-col dark:bg-slate-900 dark:text-white bg-white'>
+    <div className='duration-1000 relative min-h-screen flex flex-col dark:bg-black dark:text-white bg-white'>
     <Navbar/>
     <main className='p-1 flex-1 flex flex-col'>
     {children}
